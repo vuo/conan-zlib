e67ed09f579416f0ffa606d12646c6ede6122ac0
smokris <smokris@softpixel.com>
2018-03-31 15:53:58 -0400

    Support building on Linux.

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ class ZLibConan(ConanFile):
     package_version = '2'
     version = '%s-%s' % (source_version, package_version)
 
-    requires = 'llvm/3.3-1@vuo/stable'
+    requires = 'llvm/3.3-2@vuo/stable'
     settings = 'os', 'compiler', 'build_type', 'arch'
     url = 'http://zlib.net/'
     license = 'http://zlib.net/zlib_license.html'
@@ -15,21 +15,56 @@ class ZLibConan(ConanFile):
     source_dir = 'zlib-%s' % source_version
     install_dir = '_install'
 
+    def requirements(self):
+        if platform.system() == 'Linux':
+            self.requires('patchelf/0.9@vuo/stable')
+        elif platform.system() != 'Darwin':
+            raise Exception('Unknown platform "%s"' % platform.system())
+
     def source(self):
         tools.get('http://www.zlib.net/fossils/zlib-%s.tar.gz' % self.source_version,
                   sha256='36658cb768a54c1d4dec43c3116c27ed893e88b02ecfcb44f2166f9c0b7f2a0d')
 
+        # zlib.h contains the license in the first block comment.
+        # Truncate it at the end of that comment.
+        self.run('cp %s/zlib.h %s/%s.txt' % (self.source_dir, self.source_dir, self.name))
+        self.run('sed "/\*\//q" %s/README' % self.source_dir)
+
     def build(self):
         with tools.chdir(self.source_dir):
-            self.run('CC=%s CFLAGS="-Oz -mmacosx-version-min=10.10" LDFLAGS="-mmacosx-version-min=10.10 -Wl,-headerpad_max_install_names -Wl,-install_name,@rpath/libz.dylib" ./configure --prefix=../%s --archs="-arch x86_64" --64'
-                     % (self.deps_cpp_info['llvm'].rootpath + '/bin/clang',
-                        self.install_dir))
-            self.run('make')
-            self.run('make install')
+            cflags = '-Oz'
+            ldflags = '-Oz'
+
+            if platform.system() == 'Darwin':
+                cflags += ' -mmacosx-version-min=10.10'
+                ldflags += ' -mmacosx-version-min=10.10 -Wl,-headerpad_max_install_names -Wl,-install_name,@rpath/libz.dylib'
+
+            env_vars = {
+                'CC' : self.deps_cpp_info['llvm'].rootpath + '/bin/clang',
+                'CXX': self.deps_cpp_info['llvm'].rootpath + '/bin/clang++',
+                'CFLAGS': cflags,
+                'LDFLAGS': ldflags,
+            }
+            with tools.environment_append(env_vars):
+                self.run('./configure --prefix=../%s --archs="-arch x86_64" --64' % self.install_dir)
+                self.run('make')
+                self.run('make install')
+
+        with tools.chdir(self.install_dir):
+            if platform.system() == 'Linux':
+                patchelf = self.deps_cpp_info['patchelf'].rootpath + '/bin/patchelf'
+                self.run('%s --set-soname libz.so lib/libzmq.so' % patchelf)
 
     def package(self):
+        if platform.system() == 'Darwin':
+            libext = 'dylib'
+        elif platform.system() == 'Linux':
+            libext = 'so'
+
         self.copy('*.h', src='%s/include' % self.install_dir, dst='include/zlib')
-        self.copy('libz.dylib', src='%s/lib' % self.install_dir, dst='lib')
+        self.copy('libz.%s' % libext, src='%s/lib' % self.install_dir, dst='lib')
+
+        self.copy('%s.txt' % self.name, src=self.source_dir, dst='license')
 
     def package_info(self):
         self.cpp_info.libs = ['z']
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -8,17 +8,17 @@ class ZLibTestConan(ConanFile):
         self.run('qbs -f "%s"' % self.source_folder)
 
     def imports(self):
-        self.copy('*',       src='bin', dst='bin')
-        self.copy('*.dylib', dst='lib', src='lib')
-        self.copy('*.so',    dst='lib', src='lib')
+        self.copy('*', src='bin', dst='bin')
+        self.copy('*', dst='lib', src='lib')
 
     def test(self):
-        self.run('qbs run')
+        self.run('qbs run -f "%s"' % self.source_folder)
 
         # Ensure we only link to system libraries and our own libraries.
         if platform.system() == 'Darwin':
-            self.run('! (otool -L lib/*.dylib | grep -v "^lib/" | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
+            self.run('! (otool -L lib/libz.dylib | grep -v "^lib/" | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
+            self.run('! (otool -l lib/libz.dylib | grep -A2 LC_RPATH | cut -d"(" -f1 | grep "\s*path" | egrep -v "^\s*path @(executable|loader)_path")')
         elif platform.system() == 'Linux':
-            self.run('! (ldd lib/*.so | grep -v "^lib/" | grep "/" | egrep -v "\s/lib64/")')
+            self.run('! (ldd lib/libz.so | grep "/" | egrep -v "(\s(/lib64/|(/usr)?/lib/x86_64-linux-gnu/)|test_package/build)")')
         else:
             raise Exception('Unknown platform "%s"' % platform.system())
